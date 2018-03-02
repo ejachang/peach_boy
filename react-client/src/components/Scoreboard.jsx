6f645ee15f154641b6bb403473d786147ed7f446
Angela Chang <ajkchang@gmail.com>
2018-03-01 22:11:14 -0800

    updated scoreboard font padding

--- react-client/src/components/Scoreboard.jsx
+++ react-client/src/components/Scoreboard.jsx
@@ -3,15 +3,13 @@ import ListItem from './ListItem.jsx';
 
 const Scoreboard = (props) => (
   <div className="peachFont peachScoreboard">
-  <div>My name is:</div>
-    <p></p>
-    The peach has been clicked { props.peachClickCount } times.
-    <p></p>
-    You have { props.achievements.length } achievements.
-    <p></p>
+  <div className="peachScorefont">My name is:</div>
+  {/* avatar here */}
+  <div className="peachScorefont"> The peach has been clicked { props.peachClickCount } times.</div>
+  <div className="peachScorefont"> You have { props.achievements.length } achievements.</div>
     {/* Next achievement: {props.currentPeachStatus[props.aidx]} */}
-    <h4> Achievements </h4>
-      {/* { props.achievements.map(achievement => <ListItem achievement={achievement}/>)} */}
+  <h4 className="peachScorefont"> Achievements </h4>
+  {/* { props.achievements.map(achievement => <ListItem achievement={achievement}/>)} */}
   </div>
 )
 
