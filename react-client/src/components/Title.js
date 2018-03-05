73f1939bf4b6598d7d3143c3849b3c3034336e56
Angela Chang <ajkchang@gmail.com>
2018-03-04 18:55:15 -0800

    added Title comp

---
+++ react-client/src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Title = () => (
+  <div className="peachHeader">
+    <h1 className="peachFont peachTitle">Son of a Peach</h1>
+    <h5 className="peachFont peachSubtitle">click the peach to free him!</h5>
+  </div>
+);
+
+export default Title;
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import Title from './components/Title';
 import Scoreboard from './components/Scoreboard';
 import Comic from './components/Comic';
 import PeachImage from './components/PeachImage';
 import Profile from './components/Profile';
-// import fakeDBS from './fakeDropboxStorage.js';
 
 const app = document.getElementById('app');
 
 ReactDOM.render(
-  <div>
-    <div className="peachHeader">
-      <h1 className="peachFont peachTitle">Son of a Peach</h1>
-      <h5 className="peachFont peachSubtitle">click the peach to free him!</h5>
-    </div>
+  <Provider>
+    <Title/>
     <Scoreboard/>
     <PeachImage/>
     <Comic/>
     <Profile/>
-  </div>, app
+  </Provider>, app
 );
