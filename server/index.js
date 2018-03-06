bbd3f6bc34f622127354ae4ad7560df0560e1d4e
Angela Chang <ajkchang@gmail.com>
2018-03-05 20:33:54 -0800

    removed comments in server

--- server/index.js
+++ server/index.js
@@ -8,16 +8,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/../src/dist'));
 
-// app.get('/achievements', function (req, res) {
-//   items.selectAll(function(err, data) {
-//     if (err) {
-//       res.sendStatus(500);
-//     } else {
-//       res.json(data);
-//     }
-//   });
-// });
-
 app.listen(8000, function() {
   console.log('listening on port 8000!');
 });
