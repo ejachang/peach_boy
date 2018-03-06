ebc039c45b9238df863b89e5eccea3fef3f2bb38
Angela Chang <ajkchang@gmail.com>
2018-03-05 21:31:35 -0800

    added Header comp

---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="peachHeader">
+    <h1 className="peachFont peachTitle">Son of a Peach</h1>
+    <h5 className="peachFont peachSubtitle">click the peach to free him!</h5>
+  </div>
+);
+
+export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import $ from 'jquery';
+import Header from './components/Header';
 import Scoreboard from './components/Scoreboard';
 import Comic from './components/Comic';
 import PeachImage from './components/PeachImage';
@@ -11,13 +11,10 @@ const app = document.getElementById('app');
 
 ReactDOM.render(
   <div>
-    <div className="peachHeader">
-      <h1 className="peachFont peachTitle">Son of a Peach</h1>
-      <h5 className="peachFont peachSubtitle">click the peach to free him!</h5>
-    </div>
-    <Scoreboard/>
-    <PeachImage/>
-    <Comic/>
-    <Profile/>
+    <Header />
+    <Scoreboard />
+    <PeachImage />
+    <Comic />
+    <Profile />
   </div>, app
 );
