b810315a45d10a2cf7dce2f55eacdef5810ca231
Angela Chang <ajkchang@gmail.com>
2018-03-05 21:29:20 -0800

    updated server

--- server/index.js
+++ server/index.js
@@ -6,17 +6,7 @@ var items = require('../database-mysql');
 
 var app = express();
 
-app.use(express.static(__dirname + '/../react-client/dist'));
-
-// app.get('/achievements', function (req, res) {
-//   items.selectAll(function(err, data) {
-//     if (err) {
-//       res.sendStatus(500);
-//     } else {
-//       res.json(data);
-//     }
-//   });
-// });
+app.use(express.static(__dirname + '/../client/dist'));
 
 app.listen(8000, function() {
   console.log('listening on port 8000!');
