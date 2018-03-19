dab2312f316df7a3fd9a25ba236f564003a6258b
Angela Chang <ajkchang@gmail.com>
2018-03-19 00:03:17 -0700

    updated stle

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Header = () => (
   <div className="peachHeader">
-    <h1 className="peachFont peachTitle">Son of a Peach</h1>
+    <div className="peachSide">
+      <h1 className="peachFont peachTitle">Son of a Peach</h1>
+    </div>
+    <div className="peachSide">
+    </div>
     <h5 className="peachFont peachSubtitle">click the peach to free him!</h5>
   </div>
 );
