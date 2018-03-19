d073f1f4de64f0b591f40aa7b92a93ab33d83ac1
Angela Chang <ajkchang@gmail.com>
2018-03-19 12:59:36 -0700

    updated achievements in scoreboard

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import AchievementItem from './AchievementItem';
 
-const Scoreboard = (props) => (
-  <div className="peachFont peachsbFont peachsb peachsbPosition">
+const Scoreboard = (props) => {
+  const { achievements } = props;
+  return (  <div className="peachFont peachsbFont peachsb peachsbPosition">
     <div className="peachsbFont"> The peach has been clicked 0 times.</div>
     <div className="peachsbFont"> You have 0 achievements.</div>
-    <div className="peachsbFont">Next achievement:  {/* {props.currentPeachStatus[props.aidx]} */}</div> 
-   
+    <div className="peachsbFont">Next achievement:  {achievements[0]}</div> 
     <h4 className="peachsFont peachsbSubtitle"> Achievements </h4>
     {/* { props.achievements.map(achievement => <ListItem achievement={achievement}/>)} */}
   </div>
-);
+  );
+};
 
 export default Scoreboard;
