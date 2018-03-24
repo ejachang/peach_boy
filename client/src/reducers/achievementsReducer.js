14adebd0fe456852e120aaa95b28eb48314826ab
Angela Chang <ajkchang@gmail.com>
2018-03-24 14:49:04 -0700

    updated avatar and achivements reducer

--- client/src/reducers/achievementsReducer.js
+++ client/src/reducers/achievementsReducer.js
@@ -1,16 +1,7 @@
-const achievementsReducer = ( state = {achievements: []}, action) => {
-  switch (action.type) {
-  case 'ADD_ACHIEVEMENT_TITLE': {
-    return [
-      ...state, 
-      {
-        text: action.payload.achievement,
-      }
-    ];
-  }
-  default:
-    return state; 
-  }
+const ADD_ACHIEVEMENT = 'ADD_ACHIEVEMENT';
+
+const achievementsReducer = ( initialState = [], action) => {
+  return state; 
 };
 
 export default achievementsReducer;
--- client/src/reducers/avatarReducer.js
+++ client/src/reducers/avatarReducer.js
@@ -1,22 +1,19 @@
-const avatars = {
-  avatar: [
-    './img-ref/avatar-boy.png',
-    './img-ref/avatar-girl.png',
-    './img-ref/avatar-grill.png'
-  ]
-};
+// const avatars = {
+//   avatar: [
+//     './img-ref/avatar-boy.png',
+//     './img-ref/avatar-girl.png',
+//     './img-ref/avatar-grill.png'
+//   ]
+// };
 
 const defaultAvatar = Object.freeze({
   avatar: './img-ref/avatar-girl.png'
 });
 
 const avatarReducer = () => {
-  if (action.type === 'GIRL') {
-
-  } else if (action.type === 'BOY') {
-    
-  } else if (action.type === 'GIRL') {
-
+  if (action.type) {
+    const newAvatar = [...state, action.payload];
+    return newAvatar;
   } 
   return state;
 };
