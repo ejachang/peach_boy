e70f7983381bac9f792d646369ae8b7393fb48c1
Angela Chang <ajkchang@gmail.com>
2018-04-05 20:48:56 -0700

    updating header styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,11 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="peachHeader">
-      <div className="peachSide">
-        <h1 className="peachFont peachTitle"><a href="https://en.wikipedia.org/wiki/Momotar%C5%8D">Son of a Peach</a></h1>
-      </div>
-      <div className="peachSide">
-      </div>
+      <span className="peachFont peachTitle"><a href="https://en.wikipedia.org/wiki/Momotar%C5%8D" id="peachLink">Son of a Peach</a></span>
     </div>
   );
 };
