b23f9a5e703f36279c7e644695bab764dd5d6d69
Angela Chang <ajkchang@gmail.com>
2018-04-05 20:54:57 -0700

    updated header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="peachHeader">
-      <span className="peachFont peachTitle"><a href="https://en.wikipedia.org/wiki/Momotar%C5%8D" id="peachLink">Son of a Peach</a></span>
+      <span className="peachFont peachTitle"><a href="https://en.wikipedia.org/wiki/Momotar%C5%8D">Son of a Peach</a></span>
     </div>
   );
 };
